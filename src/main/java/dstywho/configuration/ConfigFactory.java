fc173d95527f42e738be9fec5259facfd406fc35
cdtsang <cdtsang@b0d08c91-5503-0410-b283-c95c722f64d4>
2011-09-26 18:44:25 +0000

    Configurations can now take a json File.class
    git-svn-id: https://subversion.calgbapps.org/repos/dev/DustinUtils/trunk@36601 b0d08c91-5503-0410-b283-c95c722f64d4

--- src/main/java/dstywho/configuration/ConfigFactory.java
+++ src/main/java/dstywho/configuration/ConfigFactory.java
@@ -1,10 +1,27 @@
 package dstywho.configuration;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 
+import dstywho.FileUtil;
+
 public class ConfigFactory {
-    public static <T> T fromJson(String json, Class<T> clazz)
-        {
-            return new Gson().fromJson(json, clazz);
-        }
+  public static <T> T fromJson(String json, Class<T> clazz) {
+    return new Gson().fromJson(json, clazz);
+  }
+
+  public static <T> T fromJson(File file, Class<T> clazz) {
+    String json;
+    try
+      {
+        json = FileUtil.fileToString(file);
+      }
+    catch (IOException e)
+      {
+        throw new RuntimeException("problem reading file", e);
+      }
+    return fromJson(json, clazz);
+  }
 }
