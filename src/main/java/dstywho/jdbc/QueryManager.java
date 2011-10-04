9e4f1a4cadb56f13d2188505822cdcb06db248ac
cdtsang <cdtsang@b0d08c91-5503-0410-b283-c95c722f64d4>
2011-10-04 13:22:40 +0000

    update querymanager connnection urls
    git-svn-id: https://subversion.calgbapps.org/repos/dev/DustinUtils/trunk@36711 b0d08c91-5503-0410-b283-c95c722f64d4

--- src/main/java/dstywho/jdbc/QueryManager.java
+++ src/main/java/dstywho/jdbc/QueryManager.java
@@ -12,116 +12,127 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class QueryManager {
-	public static String HOST = "jdbc:oracle:thin:@cadmium.duhs.duke.edu:1521:test1";
-	public static String USERNAME = "achecker";
-	public static String PASSWORD = "calgb_0000";
-	private Connection connection;
+    public static String HOST     = "jdbc:oracle:thin:@//neptunium.duhs.duke.edu:1521/test2.duhs.duke.edu";
+    public static String USERNAME = "achecker";
+    public static String PASSWORD = "calgb_0000";
+    private Connection   connection;
 
-	public void startConnection() throws SQLException {
-		DriverManager.registerDriver(new OracleDriver());
-		connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
-	}
+    public void startConnection() throws SQLException {
+        DriverManager.registerDriver(new OracleDriver());
+        connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+    }
 
-	public Vector<String> getColumnNames(ResultSet resultSet)
-			throws SQLException {
-		ResultSetMetaData meta = resultSet.getMetaData();
-		Vector<String> columns = new Vector<String>(meta.getColumnCount());
-		for (int i = 0; i < meta.getColumnCount(); i++) {
-			columns.add(meta.getColumnName(i + 1));
-		}
-		return columns;
-	}
+    public Vector<String> getColumnNames(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData meta = resultSet.getMetaData();
+        Vector<String> columns = new Vector<String>(meta.getColumnCount());
+        for (int i = 0; i < meta.getColumnCount(); i++) {
+            columns.add(meta.getColumnName(i + 1));
+        }
+        return columns;
+    }
 
-	public Table getRows(ResultSet resultset) throws SQLException {
-		Vector<String> columnNames = getColumnNames(resultset);
-		Table table = new Table();
-		table.setColumns(columnNames);
-		while (resultset.next()) {
-			Row row = createRow(resultset, columnNames);
-			table.insertRow(row);
-		}
-		return table;
-	}
+    public Table getRows(ResultSet resultset) throws SQLException {
+        Vector<String> columnNames = getColumnNames(resultset);
+        Table table = new Table();
+        table.setColumns(columnNames);
+        while (resultset.next()) {
+            Row row = createRow(resultset, columnNames);
+            table.insertRow(row);
+        }
+        return table;
+    }
 
-	/**
-	 * @param resultset
-	 * @param columnNames
-	 * @return
-	 * @throws SQLException
-	 */
-	private Row createRow(ResultSet resultset, Vector<String> columnNames)
-			throws SQLException {
-		Row row = new Row();
-		row.setColumns(columnNames);
-		for (int i = 1; i <= columnNames.size(); i++) {
-			row.add(resultset.getString(i));
-		}
-		return row;
-	}
+    /**
+     * @param resultset
+     * @param columnNames
+     * @return
+     * @throws SQLException
+     */
+    private Row createRow(ResultSet resultset, Vector<String> columnNames) throws SQLException {
+        Row row = new Row();
+        row.setColumns(columnNames);
+        for (int i = 1; i <= columnNames.size(); i++) {
+            row.add(resultset.getString(i));
+        }
+        return row;
+    }
 
-	public Table query(String query) throws SQLException {
-		Statement statement = connection.createStatement();
-		ResultSet resultSet = null;
-		Table table;
-		try {
-			resultSet = statement.executeQuery(query.toString());
-			table = getRows(resultSet);
-			resultSet.close();
-		} finally {
-			if (resultSet != null)
-				resultSet.close();
-			closeStatement(statement);
-		}
+    public Table query(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = null;
+        Table table;
+        try {
+            resultSet = statement.executeQuery(query.toString());
+            table = getRows(resultSet);
+            resultSet.close();
+        }
+        finally {
+            if (resultSet != null)
+                resultSet.close();
+            closeStatement(statement);
+        }
 
-		return table;
+        return table;
 
-	}
+    }
 
-	public void update(String query) throws SQLException {
-		Statement statement = connection.createStatement();
-		try {
-			statement.executeUpdate(query);
-		} finally {
-			closeStatement(statement);
-		}
-	}
+    public void execute(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        try {
+            statement.execute(query);
+        }
+        finally {
+            closeStatement(statement);
+        }
+    }
 
-	/**
-	 * @param statement
-	 */
-	private void closeStatement(Statement statement) {
-		if (statement != null) {
-			try {
-				statement.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public void update(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        try {
+            statement.executeUpdate(query);
+        }
+        finally {
+            closeStatement(statement);
+        }
+    }
 
-	public static void main(String[] args) {
-		QueryManager qm = new QueryManager();
-		try {
-			qm.startConnection();
+    /**
+     * @param statement
+     */
+    private void closeStatement(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-			Table results = qm.query("Select * from participant");
-			System.out.println(results.size());
+    public static void main(String[] args) {
+        QueryManager qm = new QueryManager();
+        try {
+            qm.startConnection();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			qm.closeConnection();
-		}
-	}
+            Table results = qm.query("Select * from participant");
+            System.out.println(results.size());
 
-	public void closeConnection() {
-		if (connection != null) {
-			try {
-				connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            qm.closeConnection();
+        }
+    }
 
-	}
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
