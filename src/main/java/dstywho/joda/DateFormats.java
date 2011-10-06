0293ae5c09f57fe56a4aa8d27c51bb8ef827c8e0
cdtsang <cdtsang@b0d08c91-5503-0410-b283-c95c722f64d4>
2011-10-06 19:30:16 +0000

    renmae class
    git-svn-id: https://subversion.calgbapps.org/repos/dev/DustinUtils/trunk@36771 b0d08c91-5503-0410-b283-c95c722f64d4

--- src/main/java/dstywho/joda/DateFormats.java
+++ src/main/java/dstywho/joda/DateFormats.java
@@ -3,7 +3,7 @@ package dstywho.joda;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-public class USDateFormats {
+public class DateFormats {
     public static final DateTimeFormatter mmddyyyy = DateTimeFormat.forPattern("MM/dd/yyyy");
     public static final DateTimeFormatter mmddyy   = DateTimeFormat.forPattern("MM/dd/YY");
 }
